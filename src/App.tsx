be7a28f44d9beddc7899e6bd744a2d64611bd7ed
obomheire <zack.bello@decagon.dev>
2022-01-27 14:06:18 +0100

    Fetched data from fake api

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,37 @@
-function App() {
+import { useState } from 'react';
+import { useQuery } from 'react-query';
+// Components
+// import Item from './Item/Item';
+// import Cart from './Cart/Cart';
+import Drawer from '@material-ui/core/Drawer';
+import LinearProgress from '@material-ui/core/LinearProgress';
+import Grid from '@material-ui/core/Grid';
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
+import Badge from '@material-ui/core/Badge';
+// Styles
+import { Wrapper, StyledButton } from './App.styles';
+
+export type CartItemType = {
+  id: number;
+  category: string;
+  description: string;
+  image: string;
+  price: number;
+  title: string;
+  amount: number;
+};
+
+const getProducts = async (): Promise<CartItemType[]> => {
+ const data = await (await fetch('http://fakestoreapi.com/products')).json()
+ return data
+}
+const App = () => {
+
+  const { data, isLoading, error } = useQuery<CartItemType[]>('products', getProducts)
+
+  // console.log(data)
   return (
-    <div>Shopping Cart App</div>
+    <div className='App'>Shopping Cart App</div>
   );
 }
 
