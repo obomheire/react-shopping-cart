fbf2eab4de3faa3c869b16f6c0f0aeebfca5b6f3
obomheire <zack.bello@decagon.dev>
2022-01-27 22:14:41 +0100

    displayed product on the page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import { useState } from 'react';
 import { useQuery } from 'react-query';
 // Components
-// import Item from './Item/Item';
+import Item from './Item/Item';
 // import Cart from './Cart/Cart';
-import Drawer from '@material-ui/core/Drawer';
-import LinearProgress from '@material-ui/core/LinearProgress';
-import Grid from '@material-ui/core/Grid';
+import  { LinearProgress, Grid, Badge, Drawer}from '@material-ui/core/';
 import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
-import Badge from '@material-ui/core/Badge';
+
 // Styles
 import { Wrapper, StyledButton } from './App.styles';
 
@@ -30,8 +28,24 @@ const App = () => {
   const { data, isLoading, error } = useQuery<CartItemType[]>('products', getProducts)
 
   // console.log(data)
+  const getTotalItems = () => null
+
+  const handleAddToCart = (clickedItem: CartItemType) => null
+
+  const handleRemoveFromCart = () => null
+
+  if (isLoading) return <LinearProgress />
+  if (error) return <div>Something went wrong...</div>
   return (
-    <div className='App'>Shopping Cart App</div>
+    <Wrapper>
+          <Grid container spacing={3}>
+          {data?.map(item => (
+          <Grid item key={item.id} xs={12} sm={4}>
+            <Item item={item} handleAddToCart={handleAddToCart} />
+          </Grid>
+        ))}
+      </Grid>
+    </Wrapper>
   );
 }
 
